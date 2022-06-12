033acfbd072c4e537b3197106faa9a509d682bb9
Leandro Freire <freire.leandro@hotmail.com>
2022-06-12 19:15:45 -0300

    correccion es vocal

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -173,9 +173,9 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  if (num == 0)
+  if (numero == 0)
       return(false);
-  if (num > 0)
+  if (numero > 0)
       return('Es positivo');
     else
       return('Es Negativo');
