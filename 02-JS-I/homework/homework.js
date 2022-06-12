39dd585d105f23ab9d0c573b855a12ef71ca1d3f
Leandro Freire <freire.leandro@hotmail.com>
2022-06-12 17:55:14 -0300

    correccion 21 modulo 5

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,14 +28,15 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  
+  return (str);
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  //const res = x + y;
+  //return (res);
 }
 
 function resta(x, y) {
