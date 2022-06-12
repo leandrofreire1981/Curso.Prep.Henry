37f4e4e79fd2b522aa86769816018462343fb70c
Leandro Freire <freire.leandro@hotmail.com>
2022-06-12 18:25:33 -0300

    prueba exponente

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -85,47 +85,61 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num<90)
+      return(true);
+    else
+      return(false);
 }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num>50)
+      return(true);
+    else
+      return(false);
 }
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-  
+  const resto = x % y;
 }
 
 function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  const resto = num % 2;
+  if (resto == 0)
+      return(true);
+    else
+      return(false);
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+ const resto = num % 2;
+  if (resto == 0)
+      return(false);
+    else
+      return(true); 
 }
 
 function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  
+  return(nom * num);
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
-  
+return(num^3);
 }
 
 function elevar(num, exponent) {
