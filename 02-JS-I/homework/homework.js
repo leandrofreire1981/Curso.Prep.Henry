74ee920cda6d81c885c60dcad9ce59635aa1b6ef
Leandro Freire <freire.leandro@hotmail.com>
2022-06-12 19:20:08 -0300

    correccion 6

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -244,6 +244,8 @@ function esVocal(letra){
     {
     if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u')
       return('Es vocal');
+    else
+      return('Dato incorrecto');
     }
   else
     return('Dato incorrecto');
