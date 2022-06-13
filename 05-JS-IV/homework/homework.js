ffeac6cec4ae2bf262934f309f04ac475c6b00e2
Leandro Freire <freire.leandro@hotmail.com>
2022-06-13 19:50:05 -0300

    prueba 6

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -57,7 +57,13 @@ function nuevoUsuario(nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
-
+  const objeto =
+  {
+    nombre: nombre,
+    email: email,
+    pasword: pasword,
+  }
+return objeto;
 }
 
 function tieneEmail(usuario) {
