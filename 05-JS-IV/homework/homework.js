3f39f3ba1ce9020593e39104f2f930206fa2eeff
Leandro Freire <freire.leandro@hotmail.com>
2022-06-13 20:01:03 -0300

    prueba 12

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -101,6 +101,8 @@ function actualizarPassword(usuario, nuevaPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
   // Devuelve el objeto
   // Tu código:
+  usuario.password=nuevaPasword;
+  return (usuario);
 }
 
 function agregarAmigo(usuario, nuevoAmigo) {
