a7518dc428645a5ae2061021832e68e536ec0344
Leandro Freire <freire.leandro@hotmail.com>
2022-06-13 19:53:15 -0300

    prueba 8

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -70,6 +70,8 @@ function tieneEmail(usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
+  return (usuario.email);
+    
 }
 
 
