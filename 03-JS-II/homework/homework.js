f906fcd7d925386c63607e6dea49485aa653f7f7
Leandro Freire <freire.leandro@hotmail.com>
2022-06-13 13:11:31 -0300

    primos 1

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -146,12 +146,14 @@ function esPrimo(numero) {
   // Nota: Los números 0 y 1 NO son considerados números primos
 if (numero == 0 || numero ==1)
   return(false);
+if (numero == 2)
+  return (true);
 for (let i=2; i<numero; i++)
 {
   if (numero%i == 0)
-    return (true);
+    return (false);
 }
-return (false);
+return (true);
 }
 
 function esVerdadero(valor){
