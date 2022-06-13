82d1eeb037499f49315e306fda4ce861b852ad99
Leandro Freire <freire.leandro@hotmail.com>
2022-06-13 19:42:02 -0300

    prueba 3

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -34,13 +34,14 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+  objeto.metodo();
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
-
+return (objetoMisterioso.numeroMisterioso*5);
 }
 
 function eliminarPropiedad(objeto, unaPropiedad) {
