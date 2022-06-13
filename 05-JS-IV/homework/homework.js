a3ed9d73729df3e922ed0254e01d1b53a50352a2
Leandro Freire <freire.leandro@hotmail.com>
2022-06-13 19:29:47 -0300

    prueba 1

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,16 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  const gato = 
+  {
+    nombre: nombre,
+    edad: edad,
+    meow: function () 
+    {
+      return ('meow');
+    }
+  };
+  return (gato);
 }
 
 
@@ -14,6 +24,9 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  objeto.property = null;
+  return (objeto);
+
 }
 
 function invocarMetodo(objeto, metodo) {
