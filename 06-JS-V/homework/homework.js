16020a6842bfb7ba7d59ddf618c3a0967cedb731
Leandro Freire <freire.leandro@hotmail.com>
2022-06-14 13:47:05 -0300

    prueba 4

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -27,6 +27,10 @@ function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+  Constructor.prototype.saludar = function ()
+  {
+    return ('Hello world!');
+  };
 }
 
 function agregarStringInvertida() {
