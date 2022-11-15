d911a68b948ea9b02bae79fb3f51c4a31ba79768
Leandro Freire <freire.leandro@hotmail.com>
2022-11-15 16:41:57 -0300

    commit

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -76,12 +76,13 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
-  for (let i=0; i<array.length; i++)
+/*   for (let i=0; i<array.length; i++)
   {
     if (array[i]===elemento)
       return (true);
   }
-return(false);
+return(false); */
+return array.includes(elemento)
 }
 
 
